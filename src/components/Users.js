7c1e1a467fd43fe74dcd04be32ddc005ec3c895c
Sika <k.sofiya1313@gmail.com>
2021-01-08 22:39:13 +0200

    created Users page

---
+++ src/components/Users.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class Users extends Component {
+  state = {
+    users: []
+  }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        this.setState({
+          users: res.data
+        });
+      })
+  }
+  render() {
+    const { users } = this.state
+    const usersList = users.map(user => {
+      return (
+        <div key={user.id}>
+          <div>
+            <p><span>Name: </span>{user.name}</p>
+            <p><span>Username: </span>{user.username}</p>
+            <p><span>Email: </span>{user.email}</p>
+            <p><span>Address: </span>{user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</p>
+            <p><span>Phone: </span>{user.phone}</p>
+            <p><span>Website: </span>{user.website}</p>
+            <p><span>Company: </span>{user.company.name}, {user.company.catchPhrase}, {user.company.bs}</p>
+            <button>Posts</button>
+
+          </div>
+        </div>
+      )
+    })
+
+    return (
+      <div>
+        <div className="container">
+          {usersList}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Users
